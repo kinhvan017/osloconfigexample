1c0d740e1e2f3555f63088c177152a0b0db1919e
tungnt <tungnt@vccloud.vn>
2015-07-26 17:37:15 +0700

    add app.py, app.conf

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# osloconfigexample
+# oslo config example
 oslo config example
--- app.py
+++ app.py
@@ -1,18 +1,46 @@
-"""simple script parse command line argument and .ini configurations files."""
+"""An OpenStack library.
+
+Parsing configuration options from the command
+line and configuration files.
+"""
 
 # http://www.giantflyingsaucer.com/blog/?p=4822
 # pip install oslo.config
-# imprort oslo config
 
-from oslo.config import cfg
+from oslo_config import cfg
 
 # define section in configuration file
-opt_group = cfg.OptGroup(name='simple',
-                         title='A simple example')
-
+opt_simple_group = cfg.OptGroup(name='simple',
+                                title='A simple example')
+opt_morestuff_group = cfg.OptGroup(name='morestuff',
+                                   title='A more complex example')
 simple_opts = [
     cfg.BoolOpt('enable', default=False,
                 help=('True enables, False disables'))
 ]
 
+morestuff_opts = [
+    cfg.StrOpt('message', default='No data',
+               help=('A message')),
+    cfg.ListOpt('usernames', default=None,
+                help=('A list of usernames')),
+    cfg.DictOpt('usermetadata', default=None,
+                help=('a dict of name and job title')),
+    cfg.IntOpt('payday', default=30,
+               help=('Default payday monthly date')),
+    cfg.FloatOpt('pi', default=0.0,
+                 help=('The value of Pi'))
+]
+
 CONF = cfg.CONF
+
+CONF.register_group(opt_simple_group)
+CONF.register_opts(simple_opts, opt_simple_group)
+
+CONF.register_group(opt_morestuff_group)
+CONF.register_opts(morestuff_opts, opt_morestuff_group)
+
+if __name__ == '__main__':
+    CONF(default_config_files=['app.conf'])
+    print "morestuff - usernames: %s" % CONF.morestuff.usernames[1]
+    print "morestuff - jobtitle: %s" % CONF.morestuff.usermetadata
